70cdbb5f2fd77b36a0d08153bd392cb6a0c0e1e3
DC23 <jugglindan@gmail.com>
2024-10-15 09:16:25 +1100

    Added a new snippets file for scene code so I don't forget the scene lighting level code I just found

---
+++ scene/snippets.js
@@ -0,0 +1,12 @@
+/*
+A macro for the Foundry virtual tabletop.
+
+This file is a collection of little bits and pieces that I might find useful one day :)
+
+Foundry v12
+Version 1.0
+*/
+
+// change the scene lighting to 0.6 with animation time of 3000ms 
+// darkness level: full bright = 0, full dark = 1
+canvas.scene.update({"environment.darknessLevel":0.6},{animateDarkness:3000})
