04b26c3330673e885d041a08c83a926453365309
DC23 <jugglindan@gmail.com>
2024-10-14 18:07:31 +1100

    placeholder structure done with all the dragonbane lightsources

--- token-lights/token-lights-dragonbane.js
+++ token-lights/token-lights-dragonbane.js
@@ -12,9 +12,17 @@ const { DialogV2 } = foundry.applications.api
 // TODO: check if tokens are selected
 if (canvas.tokens.controlled.length > 0) {
   // Build the fields for the dialog
-  const lightSource = new StringField({
+  const lightSourceField = new StringField({
     label: 'Light Source:',
-    blank: false
+    blank: false,
+    hint: 'leave blank to clear the lights',
+    choices: {
+      torch: 'Torch',
+      lantern: 'Lantern or Oil Lamp',
+      candle: 'Tallow Candle',
+      magical: 'Magical Light',
+      darkness: 'Darkness'
+    }
   }).toFormGroup({}, { name: 'lightSource' }).outerHTML
 
   const subtleAnimationsField = new BooleanField({
@@ -25,14 +33,58 @@ if (canvas.tokens.controlled.length > 0) {
   ).outerHTML
 
   // now make the dialog with the above fields.
-  const data = await DialogV2.prompt({
+  const theDialog = await DialogV2.prompt({
     window: { title: 'Set Token Lighting' },
     position: { width: 400 },
-    content: lightSource + subtleAnimationsField,
+    content: lightSourceField + subtleAnimationsField,
     ok: {
       callback: (event, button) => new FormDataExtended(button.form).object
     }
   })
+
+  // I can't work out how to get rid of the blank option, so force a null selection to have
+  // the same result as 'none'
+  const lightSource = theDialog.lightSource ? theDialog.lightSource : 'none'
+
+  for (let token of canvas.tokens.controlled) {
+    ui.notifications.notify(
+      `${token.document.name}: Setting light to ${lightSource}`
+    )
+    switch (lightSource) {
+      default:
+      case 'none':
+        console.log('none')
+        token.document.update({
+          light: {
+            dim: 0,
+            bright: 0,
+            animation: { type: 'none' },
+            negative: false
+          }
+        })
+        break
+
+      case 'torch':
+        console.log('torch')
+        break
+
+      case 'lantern':
+        console.log('lantern')
+        break
+
+      case 'candle':
+        console.log('candle')
+        break
+
+      case 'magical':
+        console.log('magical')
+        break
+
+      case 'darkness':
+        console.log('darkness')
+        break
+    }
+  }
 } else {
   ui.notifications.warn('No tokens selected')
 }
