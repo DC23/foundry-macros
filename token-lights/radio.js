9f1fa82ae3d9382e02b3bef0716501a5aaeb13a3
DC23 <jugglindan@gmail.com>
2024-10-15 23:11:58 +1100

    Got the callback returning a promise with data matching the format of the other version

--- token-lights/radio.js
+++ token-lights/radio.js
@@ -2,25 +2,36 @@
 // I'm going to morph it into a form that suits the token lighting selector,
 // then graft it to the code.
 
-new foundry.applications.api.DialogV2({
-  window: { title: 'Set Token Lighting' },
-  position: { width: 500 },
-  content: `
+const { DialogV2 } = foundry.applications.api
+
+// Since I'm now returning a promise from the dialog callback, I need to handle exceptions
+// otherwise I get an uncaught exception if the dialog is dismissed without ok being clicked.
+try {
+  const theDialog = await DialogV2.prompt({
+    window: { title: 'Set Token Lighting' },
+    position: { width: 500 },
+    content: `
     <label><input type="radio" name="choice" value="none" checked>No lights</label>
     <label><input type="radio" name="choice" value="torch">Torch</label>
     <label><input type="radio" name="choice" value="lantern">Lantern or Oil Lamp</label>
     <label><input type="radio" name="choice" value="candle">Tallow Candle</label>
     <label><input type="radio" name="choice" value="magical">Magical Light</label>
   `,
-  buttons: [
-    {
-      action: 'choice',
-      label: 'Apply Lights',
-      default: true,
-      callback: (event, button, dialog) => button.form.elements.choice.value
+    ok: {
+      callback: (event, button, dialog) => {
+        return Promise.resolve({
+          lightSource: button.form.elements.choice.value
+        })
+      }
     }
-  ],
-  submit: result => {
-    ui.notifications.notify(`User picked option: ${result}`)
+  })
+
+  // If execution gets here, the dialog exited successfully
+  const lightSource = theDialog.lightSource
+
+  for (let token of canvas.tokens.controlled) {
+    ui.notifications.notify(
+      `${token.document.name}: Setting light to ${lightSource}`
+    )
   }
-}).render({ force: true })
+} catch (e) {}
