b8589068c3401f5347c93c21c6b61f35050358a4
DC23 <jugglindan@gmail.com>
2024-10-16 16:48:13 +1100

    lantern non-ui macro

---
+++ token-lights/dragonbane-non-ui-lights/dragonbane-lantern.js
@@ -0,0 +1,24 @@
+/* 
+Simple non-UI macro for setting lighting on selected tokens to a torch effect for Dragonbane.
+Matches the lantern setting of the token-lights-dragonbane.js Dialog macro.
+
+Foundry v11, v12
+Version 1.0
+*/
+for (let token of canvas.tokens.controlled) {
+  token.document.update({
+    light: {
+      color: '#a88115',
+      alpha: 0.4,
+      dim: 10, // This is the torch radius in metres or game units
+      bright: 8,  // This should be a little smaller than the dim radius
+      luminosity: 0.5,
+      animation: {
+        type: 'torch',
+        speed: 3,
+        intensity: 2
+      }
+    }
+  })
+}
+
