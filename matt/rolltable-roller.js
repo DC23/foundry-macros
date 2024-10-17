55309f9b16a61c2139dde853ca567127de78fdc7
DC23 <jugglindan@gmail.com>
2024-10-17 19:54:42 +1100

    renamed rolltable roller and made forced result argument optional

---
+++ matt/rolltable-roller.js
@@ -0,0 +1,19 @@
+/* 
+A script macro that rolls an on a roll table and displays the result to chat.
+It accepts two arguments:
+  args[0]: The RollTable UUID in the form 'RollTable.UUID'
+  args[1]: Optional. If supplied, this forces the result to a specific entry.
+
+Intended to be called from a Run Macro action on a MATT tile, so that the tile can trigger pre-defined
+table results on demand.
+
+Foundry v11, v12
+Version 1.2
+*/
+
+const table = await fromUuid(args[0])
+if (args.length > 1 && args[1] >= 1 && args[1] <= table.results.size) {
+  await table.draw({roll: new Roll(args[1].toString())});
+} else {
+  await table.draw();
+}
