ba3ef133e0c32960f7bf74891f6a4fa367dcbf4e
DC23 <jugglindan@gmail.com>
2024-10-17 22:29:15 +1100

    Adding remaining effects

---
+++ token-lights/status-effect-lights/status-effect-lantern.js
@@ -0,0 +1,40 @@
+/* 
+Macro for setting a token lighting to torch light from a status effect selection.
+Will clear all other lighting status effects from the token.
+Matches the torch setting of the token-lights-dragonbane.js Dialog macro.
+
+Foundry v11, v12
+Version 1.0
+*/
+
+// ID values obtained by first creating the effects in Condition Lab then exporting the custom effects to JSON.
+// The ID values for each effect can then be found in the JSON.
+const TORCH_ID = 'Pg7M8xPkWgBsN5QE'
+const LANTERN_ID = 'vlhmJysWhlnT37pd'
+const CANDLE_ID = 'SMpoK2V52jbX1gqE'
+const MAGICAL_ID = 'K5IlSedG8PAqzazF'
+
+// FIXME: this is wrong. For macros called from Condition Lab, I shouldn't be using the controlled tokens.
+// The token should be an argument or something.
+for (let token of canvas.tokens.controlled) {
+  // turn off the other effects for this token
+  function effectOff (id) {
+    if (token.document.hasStatusEffect(id)) token.actor.toggleStatusEffect(id)
+  }
+  ;[TORCH_ID, CANDLE_ID, MAGICAL_ID].forEach(effectOff)
+
+  token.document.update({
+    light: {
+      color: '#a88115',
+      alpha: 0.4,
+      dim: 10, // This is the light radius in metres or game units
+      bright: 8, // This should be a little smaller than the dim radius
+      luminosity: 0.5,
+      animation: {
+        type: 'torch',
+        speed: 3,
+        intensity: 2
+      }
+    }
+  })
+}
---
+++ token-lights/status-effect-lights/status-effect-magical.js
@@ -0,0 +1,41 @@
+/* 
+Macro for setting a token lighting to torch light from a status effect selection.
+Will clear all other lighting status effects from the token.
+Matches the torch setting of the token-lights-dragonbane.js Dialog macro.
+
+Foundry v11, v12
+Version 1.0
+*/
+
+// ID values obtained by first creating the effects in Condition Lab then exporting the custom effects to JSON.
+// The ID values for each effect can then be found in the JSON.
+const TORCH_ID = 'Pg7M8xPkWgBsN5QE'
+const LANTERN_ID = 'vlhmJysWhlnT37pd'
+const CANDLE_ID = 'SMpoK2V52jbX1gqE'
+const MAGICAL_ID = 'K5IlSedG8PAqzazF'
+
+// FIXME: this is wrong. For macros called from Condition Lab, I shouldn't be using the controlled tokens.
+// The token should be an argument or something.
+for (let token of canvas.tokens.controlled) {
+  // turn off the other effects for this token
+  function effectOff (id) {
+    if (token.document.hasStatusEffect(id)) token.actor.toggleStatusEffect(id)
+  }
+  ;[LANTERN_ID, CANDLE_ID, TORCH_ID].forEach(effectOff)
+
+  // now turn the required effect on
+  token.document.update({
+    light: {
+      color: '#d4d4d4',
+      alpha: 0.3,
+      dim: 10, // This is the light radius in metres or game units
+      bright: 8, // This should be a little smaller than the dim radius
+      luminosity: 0.5,
+      animation: {
+        type: 'fog',
+        speed: 3,
+        intensity: 2
+      }
+    }
+  })
+}
