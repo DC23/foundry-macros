2bb12c6afd431852ff33b253eba83551a1b12760
DC23 <jugglindan@gmail.com>
2024-10-17 22:14:46 +1100

    I'm glad to see JS has closures. This version works and will be easy to roll out for the other three lights. But first I need to fix the FIXME

--- token-lights/status-effect-lights/status-effect-torch.js
+++ token-lights/status-effect-lights/status-effect-torch.js
@@ -7,37 +7,35 @@ Foundry v11, v12
 Version 1.0
 */
 
-// Bits and pieces of API calls I might need
+// ID values obtained by first creating the effects in Condition Lab then exporting the custom effects to JSON.
+// The ID values for each effect can then be found in the JSON.
+const TORCH_ID = 'Pg7M8xPkWgBsN5QE'
+const LANTERN_ID = 'vlhmJysWhlnT37pd'
+const CANDLE_ID = 'SMpoK2V52jbX1gqE'
+const MAGICAL_ID = 'K5IlSedG8PAqzazF'
 
-// This is the list of current statuses
-// token.actor.statuses
-
-// also
-// token.document.hasStatusEffect(id)
-
-// I can toggle statuses with this call
-// await token.actor.toggleStatusEffect(statusId)
-
-// if I know the ID of the Condition Lab statuses for the lights,
-// I can ensure the other statuses are all off.
-
-// This will get me all the data from the ID that I get from the statuses.
-// I can use that to get effect names from an id, since Condition Lab tends to hide the ID
-// unless I grab it from an exported JSON file.
-// ActiveEffect.fromStatusEffect(ID)
-
-// Apply the torch lighting
-token.document.update({
-  light: {
-    color: '#a88115',
-    alpha: 0.4,
-    dim: 10, // This is the torch radius in metres or game units
-    bright: 7.2, // This should be a little smaller than the dim radius
-    luminosity: 0.5,
-    animation: {
-      type: 'torch',
-      speed: 5,
-      intensity: 2
-    }
+// FIXME: this is wrong. For macros called from Condition Lab, I shouldn't be using the controlled tokens.
+// The token should be an argument or something.
+for (let token of canvas.tokens.controlled) {
+  // turn off the other effects for this token
+  function effectOff (id) {
+    if (token.document.hasStatusEffect(id)) token.actor.toggleStatusEffect(id)
   }
-})
+  ;[LANTERN_ID, CANDLE_ID, MAGICAL_ID].forEach(effectOff)
+
+  // now turn the required effect on
+  token.document.update({
+    light: {
+      color: '#a88115',
+      alpha: 0.4,
+      dim: 10, // This is the torch radius in metres or game units
+      bright: 7.2, // This should be a little smaller than the dim radius
+      luminosity: 0.5,
+      animation: {
+        type: 'torch',
+        speed: 5,
+        intensity: 2
+      }
+    }
+  })
+}
