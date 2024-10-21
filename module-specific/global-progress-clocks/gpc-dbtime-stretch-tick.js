ba6316b93a01b9ef4d992afc2dcde68248b65242
DC23 <jugglindan@gmail.com>
2024-10-21 14:31:48 +1100

    Fixed some logic errors in error checking and clock ticks

--- module-specific/global-progress-clocks/gpc-dbtime-stretch-tick.js
+++ module-specific/global-progress-clocks/gpc-dbtime-stretch-tick.js
@@ -30,37 +30,65 @@ function validate_clock (clock, name, segments) {
 
 /**
  * Gets a validated timekeeping progress clock by name.
- * @param {String} name 
- * @param {Number} segments 
+ * @param {String} name
+ * @param {Number} segments
  * @returns The validated Global Progress Clocks clock object, or null if a valid clock could not be found.
  */
 function getValidClock (name, segments) {
+  var clock = null
   try {
-    const clock = window.clockDatabase.getName(name)
-
-    try {
-      validate_clock(clock, name, segments)
-    } catch (error) {
-      ui.notifications.error(error)
-      return null
-    } finally {
-      return clock
-    }
+    clock = window.clockDatabase.getName(name)
   } catch (error) {
     ui.notifications.error(
       'The Global Progress Clocks module is probably not loaded'
     )
     return null
   }
+
+  try {
+    validate_clock(clock, name, segments)
+  } catch (error) {
+    ui.notifications.error(error)
+    return null
+  }
+
+  return clock
+}
+
+function tick (clock) {
+  window.clockDatabase.update({ id: clock.id, value: clock.value + 1 })
+}
+
+function reset (clock, value = 1) {
+  window.clockDatabase.update({ id: clock.id, value: value })
 }
 
 const stretch = getValidClock('Stretch', 24)
 const shift = getValidClock('Shift', 4)
-const day = getValidClock('Day', 7)
+const day = getValidClock('Day', 30)
 
-// tick within current stretch
-// tick will end the stretch and advance the shift
-// advancing the shift will advance the day
-
-// The clock update code
-//window.clockDatabase.update({ id: clock.id, value: clock.value + 1 });
+if (stretch && shift && day) {
+  if (stretch.value < stretch.max) {
+    // tick within current stretch
+    console.log('stretch tick')
+    tick(stretch)
+  } else {
+    if (shift.value < shift.max) {
+      // tick will end the stretch and advance the shift
+      console.log('new shift')
+      reset(stretch)
+      tick(shift)
+    } else {
+      // it's a new day!
+      console.log('new day')
+      reset(stretch)
+      reset(shift)
+      if (day.value < day.max) {
+        //reset stretch and shift, tick the day
+        tick(day)
+      } else {
+        reset(day)
+      }
+    }
+  }
+}
