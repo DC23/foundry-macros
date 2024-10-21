47be0fe25826d6b077f7b7d85635ae2829f385d6
DC23 <jugglindan@gmail.com>
2024-10-21 13:18:52 +1100

    Starting with some basic validation of the required stretch, shift, and day clocks

---
+++ module-specific/global-progress-clocks/gpc-dbtime-stretch-tick.js
@@ -0,0 +1,66 @@
+/*
+A macro for the Foundry virtual tabletop.
+
+Ticks the Dragonbane Timekeeping clocks forward by 1 stretch.
+
+Dependencies: 
+  - Global Progress Clocks >= 0.4.5
+
+Foundry v12
+Version 1.0
+*/
+
+/**
+ * Validates a Global Progress Clock clock.
+ * Since so much of these scripts depend on these clocks being configured correctly, but
+ * that configuration is outside the control of the scripts, the least I can do is check
+ * that it's right.
+ * @param {Object} clock The clock to check.
+ * @param {String} name The clock name.
+ * @param {Number} segments The expected number of segments.
+ */
+function validate_clock (clock, name, segments) {
+  if (!clock) throw new Error(`DBTime: Global Progress Clock '${name}' missing`)
+
+  if (clock.max != segments)
+    throw new Error(
+      `DBTime: Global Progress Clock '${name}' has ${clock.max} segments, it requires ${segments}`
+    )
+}
+
+/**
+ * 
+ * @param {String} name 
+ * @param {Number} segments 
+ * @returns The validated Global Progress Clocks clock object, or null if a valid clock could not be found.
+ */
+function getValidClock (name, segments) {
+  try {
+    const clock = window.clockDatabase.getName(name)
+
+    try {
+      validate_clock(clock, name, segments)
+    } catch (error) {
+      ui.notifications.error(error)
+      return null
+    } finally {
+      return clock
+    }
+  } catch (error) {
+    ui.notifications.error(
+      'The Global Progress Clocks module is probably not loaded'
+    )
+    return null
+  }
+}
+
+const stretch = getValidClock('Stretch', 24)
+const shift = getValidClock('Shift', 4)
+const day = getValidClock('Day', 7)
+
+// tick within current stretch
+// tick will end the stretch and advance the shift
+// advancing the shift will advance the day
+
+// The clock update code
+//window.clockDatabase.update({ id: clock.id, value: clock.value + 1 });
