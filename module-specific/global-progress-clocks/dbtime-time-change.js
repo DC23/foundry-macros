f8ba4f6cec3e2e9d3a967a071642c73c14c4759f
DC23 <jugglindan@gmail.com>
2024-10-23 23:06:18 +1100

    adding example code that can post the current time to chat at the start of each hour or shift

--- module-specific/global-progress-clocks/dbtime-time-change.js
+++ module-specific/global-progress-clocks/dbtime-time-change.js
@@ -10,16 +10,16 @@ clocks have changed, this script is always called when the time is changed in an
 Foundry v12
 Version 1.0
 */
-
 /**
- * This code will execute whenever the time changes to an even hour, even if you don't have
- * the optional Hour clock enabled, while anything placed into dbtime-hour-change 
- * is only called when the Hour clock is in use.
+ * This code will execute whenever the time changes to an even hour, even if you don't have the optional Hour clock
+ * enabled, while anything placed into dbtime-hour-change is only called when the Hour clock
+ * is in use.
  */
-if (scope.newTime.stretch % 4 === 0) {
+ if (scope.newTime.stretch % 4 === 0) {
     // post the time to chat
-    const macro = game.macros.getName('dbtime-engine')
-    await macro.execute({ mode: 'tell', includeDay: true })
-
-    // you can add anything else you want here
+    let content = `It's ${scope.newTime.time} on day ${scope.newTime.day + 1}` // display in 1-based days
+    ChatMessage.create({
+        speaker: { actor: game.user.id },
+        content: content,
+    })
 }
