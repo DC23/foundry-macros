308efe512c1628ec01f48a8fb9de1eb935579021
DC23 <jugglindan@gmail.com>
2024-10-24 19:39:52 +1100

    starting to sketch in the algorithm for lighting automation

--- module-specific/global-progress-clocks/dbtime-time-change.js
+++ module-specific/global-progress-clocks/dbtime-time-change.js
@@ -16,7 +16,8 @@ Version 1.1
  * enabled, while anything placed into dbtime-hour-change is only called when the Hour clock
  * is in use.
  */
- if (scope.time.stretch % 4 === 0) {
+/*
+if (scope.time.stretch % 4 === 0) {
     // it's the start of a new hour
 
     if (scope.time.stretch === 0 && scope.time.shift === 0) {
@@ -31,13 +32,20 @@ Version 1.1
         content: content,
     })
 }
+*/
 
 /**
  * Scene Lighting Automation
  *
- * Dawn Sequence
+ * Shift 0 is 6am to 12pm
+ * Shift 1 is 12pm to 6pm
+ * Shift 2 is 6pm to 12am
+ * Shift 3 is 12am to 6am
  *
- * NIGHT_DARKNESS
+ * Shifts 0 & 1 are daylight shifts, while 2 & 3 are night shifts.
+ * Sunrise is from 6am to 7am
+ * Sunset is from 6pm to 7pm
+ *
  *
  * 6:00 am -> shift 0, stretch 0
  * 6:15 am -> shift 0, stretch 1
@@ -45,14 +53,37 @@ Version 1.1
  * 6:45 am -> shift 0, stretch 3
  * 7:00 am -> shift 0, stretch 4
  *
- * DAY_DARKNESS
- *
- * Sunset Sequence
  * 6:00 pm -> shift 2, stretch 0
  * 6:15 pm -> shift 2, stretch 1
  * 6:30 pm -> shift 2, stretch 2
  * 6:45 pm -> shift 2, stretch 3
  * 7:00 pm -> shift 2, stretch 4
  *
- * NIGHT_DARKNESS
  */
+
+// The daytime scene darkness level
+const DAY_SCENE_DARKNESS = 0.0
+
+// The nightime scene darkness level
+const NIGHT_SCENE_DARKNESS = 1.0
+
+// Handle the day shift
+if (scope.time.shift === 0 || scope.time.shift === 1) {
+    // Is it dawn?
+    if (scope.time.stretch >= 0 && scope.time.stretch <= 4) {
+        // interpolate the darkness value based on the current stretch
+        console.log('dawn')
+    } else {
+        // it's not dawn, but it is day
+        // we need to ensure that the current scene darkness level matches the day setting
+    }
+} else if (scope.time.shift === 2 || scope.time.shift === 3) {
+    // Is it sunset?
+    if (scope.time.stretch >= 0 && scope.time.stretch <= 4) {
+        // interpolate the darkness value based on the current stretch
+        console.log('sunset')
+    } else {
+        // it's not sunset, but it is night
+        // we need to ensure that the current scene darkness level matches the night setting
+    }
+}
