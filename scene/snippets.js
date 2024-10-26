55c8c6cfe65c37baa7b6870a8ae497ff547c8663
DC23 <jugglindan@gmail.com>
2024-10-27 09:13:11 +1100

    Fixed the mistake in the ambient light update, where I was updating the client-side lights rather than the server-side documents. Newb mistakes!

--- scene/snippets.js
+++ scene/snippets.js
@@ -7,21 +7,22 @@ Foundry v12
 Version 1.0
 */
 
-/** 
+/**
  * change the current scene lighting to 0.6 with animation time of 3000ms
  * darkness level: full bright = 0, full dark = 1
-*/
+ */
 await canvas.scene.update(
     { 'environment.darknessLevel': 0.6 },
     { animateDarkness: 3000 }
 )
 
 /**
- * Update all lights at once on a scene based on a tagger tag.  
+ * Update all lights at once on a scene based on a tagger tag.
  * This is a nice workaround for the lack of multiselect and edit on lights.
- * Can be used to edit any properties. I needed to change animation intensity on a swamp map,
- * so that's what I've saved here :)
- * 
+ * Can be used to edit any properties. 
+ * I needed to change animation intensity, and the min/max darkness settings 
+ * on a swamp map, so that's what I've saved here :)
+ *
  * Note the first test is for the presence of flags.tagger.tags, which is absent if a light has no
  * tags applied at all.
  */
@@ -31,7 +32,11 @@ const lights = canvas.scene.lights.filter(
 const updates = lights.map(l => ({
     _id: l.id,
     'config.animation.intensity': 5,
+    'config.darkness.min': 0.2,
+    'config.darkness.max': 0.7,
 }))
 if (updates) {
-    await canvas.scene.lights.update(updates)
+    await AmbientLightDocument.updateDocuments(updates, {
+        parent: canvas.scene,
+    })
 }
