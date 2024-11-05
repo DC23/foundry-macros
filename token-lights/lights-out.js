0975b7368f1930204b6e331fde2f9fa51867bf37
DC23 <jugglindan@gmail.com>
2024-11-05 20:55:50 +1100

    updating lights out to reset luminosity, alpha, and angle.

--- token-lights/lights-out.js
+++ token-lights/lights-out.js
@@ -2,13 +2,16 @@
 Simple macro for clearing lights from the selected tokens.
 
 Foundry v11, v12
-Version 1.0
+Version 1.1
 */
 for (let token of canvas.tokens.controlled) {
     token.document.update({
         light: {
             dim: 0,
             bright: 0,
+            angle: 360,
+            alpha: 0.5,
+            luminosity: 0.5,
             animation: { type: 'none' },
             negative: false,
         },
