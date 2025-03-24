af634b50b6d662af502f6ec866779e617eb02f8e
DC23 <DC23@users.noreply.github.com>
2025-03-24 21:36:39 +1100

    now saving the full time object in the flag

--- module-specific/mgt2e/Start Jump.js
+++ module-specific/mgt2e/Start Jump.js
@@ -8,7 +8,7 @@ Requires:
     - Easy Timekeeping module v1.2.1+
 
 Foundry v12
-Version 1.5
+Version 1.6
 */
 
 const jumpDurationHours = await new Roll('6d6+148').evaluate()
@@ -25,7 +25,7 @@ function formatTravellerDate(day, year) {
 }
 
 // Set the end time of the jump to the global flag so it can be checked in the time change handler macro
-game.user.setFlag('world', 'ravens-call-current-jump-end', jumpEndTime)
+game.user.setFlag('world', 'ravens-call-current-jump-end', jumpEnd)
 
 let chatContent = `The jump is calculated to complete in ${jumpDuration.days} days, ${jumpDuration.hours} hours on <b>${formatTravellerDate(jumpEndDate.day, jumpEndDate.year)}</b> at <b>${jumpEnd.hours.toString().padStart(2, '0')}:${jumpEnd.minutes.toString().padStart(2, '0')}</b>`
 let chatData = {
