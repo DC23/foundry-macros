295fa908224f4ba46c9de97ec1a8c901cb79fa0e
DC23 <DC23@users.noreply.github.com>
2025-03-24 21:40:04 +1100

    implemented jump handling for time changed handler

--- module-specific/mgt2e/time-changed-handler.js
+++ module-specific/mgt2e/time-changed-handler.js
@@ -2,18 +2,32 @@
 Easy Timekeeping time changed handler for MgT2e game system.
 
 - Todo: Keeps the MgT2e current day and year updated to the Easy Timekeeping year and day
-- Todo: Calls the Show Traveller Date macro if more than 6 days have elapsed
+- Updates jump progress
 
 Requires: 
  - Mongoose Traveller 2e system 0.11.10+
  - Easy Timekeeping module v1.2.1+
 
 Foundry v12
-Version 0.1
+Version 1.4
 */
 
 // If the day has changed, then make updates to the Traveller system day and year settings
-if (scope.time.days != scope.oldTime.days) {
-    //console.log('Time Changed Handler: the day has changed. Was %s, Now %s', scope.oldTime.days, scope.time.days)
+if (time.days != oldTime.days) {
     //console.log("MgT2e Date: %s", toTravellerDate(game.settings.get('mgt2e', 'currentDay'), game.settings.get('mgt2e', 'currentYear')))
+}
+
+const jumpEndTime = game.user.getFlag('world', 'ravens-call-current-jump-end')
+if (jumpEndTime) {
+    /* only send an automatic Jump progress message if more than a day has elapsed 
+       or if less than 1 day remains to the end of the jump
+       or if time has moved past the end time of the jump
+    */
+    if (
+        time.days != oldTime.days ||
+        jumpEndTime.totalMinutes - time.totalMinutes < 60 * 24
+    ) {
+        const macro = game.macros.getName('show-jump-progress')
+        const jumpEndDate = await macro.execute({ time: time })
+    }
 }
