2045520e58b5c7f33c530bd958896a6892c79c11
DC23 <DC23@users.noreply.github.com>
2025-03-25 09:12:17 +1100

    Placeholder for end jump macro

---
+++ module-specific/mgt2e/End Jump.js
@@ -0,0 +1,14 @@
+/*
+If there is a jump in progress and the jump end time is in the future, then:
+- set time to the end time of the jump
+- clear the jump flag
+
+Requires: 
+    - Mongoose Traveller 2e system 0.11.10+
+    - Easy Timekeeping module v1.2.1+
+
+Foundry v12
+Version 0.0
+*/
+
+// TODO
