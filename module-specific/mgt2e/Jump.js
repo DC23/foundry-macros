d633d6c1cc529c385eb6e88ce6f932c1bc48cd84
DC23 <DC23@users.noreply.github.com>
2025-03-25 09:03:15 +1100

    Formatting updates

--- module-specific/mgt2e/Jump.js
+++ module-specific/mgt2e/Jump.js
@@ -1,19 +1,27 @@
 /*
-Performs an instant jump, advancing time. This is useful if no gameplay is taking place
+Performs an instant jump, advancing time. 
+This is useful if no gameplay is taking place
 inside the ship during the jump itself.
-Note that the flag used by the show-jump-progress macro and related functionality is not set.
+Note that the flag used by the show-jump-progress
+macro and related functionality is not set.
 
 Foundry v12
-Version 1.1
+Version 1.3
 */
 let jumpDurationHours = await new Roll('6d6+148').evaluate()
 console.log('Jump hours', jumpDurationHours.total)
 
-game.modules.get('jd-easytimekeeping').api.increment({hours: jumpDurationHours.total})
+game.modules
+    .get('jd-easytimekeeping')
+    .api.increment({ hours: jumpDurationHours.total })
 
 const chatContent = `<b>Jump completed in ${jumpDurationHours.total} hours.</b>`
 const chatData = {
-    speaker: { actor:  canvas.tokens.controlled[0] ? canvas.tokens.controlled[0].actor : game.user.id },
+    speaker: {
+        actor: canvas.tokens.controlled[0]
+            ? canvas.tokens.controlled[0].actor
+            : game.user.id,
+    },
     content: chatContent,
 }
 
