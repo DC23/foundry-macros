fdf0ab4dbbce9c818a967c15147f61857724ca6b
DC23 <DC23@users.noreply.github.com>
2025-03-24 18:23:52 +1100

    Saving jump end time to a flag on the current user

--- module-specific/mgt2e/Start Jump.js
+++ module-specific/mgt2e/Start Jump.js
@@ -8,7 +8,7 @@ Requires:
     - Easy Timekeeping module v1.2.1+
 
 Foundry v12
-Version 1.4
+Version 1.5
 */
 
 const jumpDurationHours = await new Roll('6d6+148').evaluate()
@@ -23,6 +23,9 @@ const jumpEndDate = await macro.execute({'time': jumpEnd})
 function formatTravellerDate(day, year) {
     return `${day.toString().padStart(3, '0')}-${year}`
 }
+
+// Set the end time of the jump to the global flag so it can be checked in the time change handler macro
+game.user.setFlag('world', 'ravens-call-current-jump-end', jumpEndTime)
 
 let chatContent = `The jump is calculated to complete in ${jumpDuration.days} days, ${jumpDuration.hours} hours on <b>${formatTravellerDate(jumpEndDate.day, jumpEndDate.year)}</b> at <b>${jumpEnd.hours.toString().padStart(2, '0')}:${jumpEnd.minutes.toString().padStart(2, '0')}</b>`
 let chatData = {
