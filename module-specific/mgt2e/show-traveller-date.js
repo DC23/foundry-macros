00ab8afa445ad084e3c077668ceac4105df2be73
DC23 <DC23@users.noreply.github.com>
2025-03-24 11:42:18 +1100

    Minor formatting changes

--- module-specific/mgt2e/show-traveller-date.js
+++ module-specific/mgt2e/show-traveller-date.js
@@ -2,13 +2,13 @@
 Displays the current time and date to chat formatted as a Traveller time and date.
 
 Assumes that the Easy Timekeeping Weekday Settings are already set to:
-- 7 days per week
-- Days named: Oneday, Twoday, Threeday, Fourday, Fiveday, Sixday, Sevenday 
+    - 7 days per week
+    - Days named: Oneday, Twoday, Threeday, Fourday, Fiveday, Sixday, Sevenday 
 as described on page 114 of Mg2e Traveller Companion page 114 where the conventions for the reckoning of time are described.
 
 Requires: 
- * Mongoose Traveller 2e system 0.11.10+
- * Easy Timekeeping module v1.2.1+
+    - Mongoose Traveller 2e system 0.11.10+
+    - Easy Timekeeping module v1.2.1+
 
 Foundry v12
 Version 1.1
